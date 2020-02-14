609f62ff766d34bd9a657de4a110ef3c2d5e25e2
corentingcholet <corentingcholet@gmail.com>
2020-02-14 13:53:36 +0100

    Site

---
+++ campus_contest/config/mail.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+    /*
+    |--------------------------------------------------------------------------
+    | SMTP Host Port
+    |--------------------------------------------------------------------------
+    |
+    | This is the SMTP port used by your application to deliver e-mails to
+    | users of the application. Like the host we have set this value to
+    | stay compatible with the Mailgun e-mail application by default.
+    |
+    */
+    'port' => env('MAIL_PORT', 587),
+    /*
+    |--------------------------------------------------------------------------
+    | Global "From" Address
+    |--------------------------------------------------------------------------
+    |
+    | You may wish for all e-mails sent by your application to be sent from
+    | the same address. Here, you may specify a name and address that is
+    | used globally for all e-mails that are sent by your application.
+    |
+    */
+    'from' => ['address' => 'corentingcholet@gmail.com', 'name' => 'hawle'],
+    /*
+    |--------------------------------------------------------------------------
+    | E-Mail Encryption Protocol
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the encryption protocol that should be used when
+    | the application send e-mail messages. A sensible default using the
+    | transport layer security protocol should provide great security.
+    |
+    */
+    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+
+];
